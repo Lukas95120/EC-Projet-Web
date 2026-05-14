d142c898f7cffa21b216d454a3050b87d683c1a3
Lukas <lukasestragnat@gmail.com>
2026-05-14 23:42:42 +0200

    feat : Fix rapide

--- doc/structure.sql
+++ doc/structure.sql
@@ -1,9 +1,3 @@
-CREATE DATABASE IF NOT EXISTS gamestats
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
-USE gamestats;
-
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
